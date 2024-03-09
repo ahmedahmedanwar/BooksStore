6bce27a56dde21d63ac1baf89fcc784028a045f9
ahmedahmedanwar <ahmed.mido20101234@gmail.com>
2024-03-10 00:46:29 +0200

    Adding animationClass to the splash Screen.

---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      
+    );
+  }
+}
